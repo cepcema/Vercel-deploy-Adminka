65d2010ab3619eb34c6a2e9aeb1bae40cd5cb155
cepcema <49753804+cepcema@users.noreply.github.com>
2023-01-30 20:01:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 
 app.use(cors());
 
-// app.all("/", function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next();
-// });
+ app.all("/", function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     next();
+ });
 
 app.get(`/`, async(req, res, next) => {
     const auth = new google.auth.GoogleAuth({
